2c9a0542655475ee2b92b315b7d25abd944d1667
Eddy <kimkyunghun3@naver.com>
2022-04-11 23:35:48 +0900

    🩹[CHORE] : decode한 데이터와 작성한 decode 구조체 비교 네이밍 수정

--- Expo1900/Expo1900Tests/Expo1900Tests.swift
+++ Expo1900/Expo1900Tests/Expo1900Tests.swift
@@ -30,9 +30,8 @@ class Expo1900Tests: XCTestCase {
         XCTAssertNoThrow(try? decoder.decode(WorldFairPoster.self, from: data))
     }
     
-    func test_mock_file() {
+    func test_decode_Json_data_compare_to_decode_WorldFairPoster_file() {
         let decoder = JSONDecoder()
-        
         guard let asset = NSDataAsset(name: "exposition_universelle_1900", bundle: .main),
             let expo = try? decoder.decode(WorldFairPoster.self, from: asset.data)
         else {
